a61c70ec0c0f1d477c8d4b6fe27d46408711271d
Tiwa Babalola <tbabalola@getcarbon.co>
2022-04-01 00:49:14 +0100

    feat : added coffee code

---
+++ lib/coffee.dart
@@ -0,0 +1,86 @@
+import 'package:coffeshop/detail_page.dart';
+import 'package:flutter/material.dart';
+
+class Coffee extends StatelessWidget {
+  const Coffee({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          SizedBox(
+            height: size.height,
+            width: size.height,
+            child: Image.asset("images/background.jpg", fit: BoxFit.cover),
+          ),
+          Container(
+            padding: const EdgeInsets.all(40),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Theme.of(context).primaryColor,
+                  Theme.of(context).primaryColor.withOpacity(.4)
+                ],
+              ),
+            ),
+            child: Image.asset('images/coffee.png'),
+          ),
+          const Positioned(
+            top: 70,
+            child: Text(
+              "ITS GREATE DAY FOR",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Color(0xffB98C53),
+              ),
+            ),
+          ),
+          const Positioned(
+            top: 95,
+            child: Text(
+              "Coffee",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 50,
+            child: SizedBox(
+              height: 60,
+              width: 150,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                color: const Color(0xffB98C53),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => DetailPage(),
+                  ),
+                ),
+                child: const Text(
+                  "Order",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/detail_page.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SizedBox(
+        height: size.height,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(30),
+              alignment: Alignment.topCenter,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              height: 290,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("images/cappuccino.jpg"),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 250,
+              child: Container(
+                padding: const EdgeInsets.all(40),
+                width: size.width,
+                height: size.height,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const Text(
+                      "COFFEE",
+                      style: TextStyle(fontSize: 17),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "cappuccino",
+                      style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.star, color: Colors.amber),
+                        Icon(Icons.star, color: Colors.amber),
+                        Icon(Icons.star, color: Colors.amber),
+                        Icon(Icons.star, color: Colors.amber),
+                        Icon(Icons.star_outline, color: Colors.amber),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      " is simply dummy text of the printing and typesetting"
+                      " industry. Lorem Ipsum has been the industry's standard dummy "
+                      "text ever since the 1500s, when an unknown printer took a galley"
+                      " of type and scrambled it to make a type specimen book. It has ",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      '\$10.90',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      alignment: Alignment.center,
+                      width: 170,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(23),
+                          border: Border.all(
+                            color: Colors.grey,
+                            style: BorderStyle.solid,
+                          )),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(onPressed: () {}, child: const Text("+")),
+                          const Text("2"),
+                          TextButton(onPressed: () {}, child: const Text("-"))
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: 55,
+                      width: double.infinity,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                        onPressed: () {},
+                        child: const Text("Order Now",style: TextStyle(
+                          fontSize: 20,
+                        ),),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
